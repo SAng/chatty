499d4a713f90bdea6b34a6e56b330e485e4e35e3
Spencer Ang <boomerandzapper@gmail.com>
2017-05-30 13:14:16 -0700

    Implemented React Made the whole app use react and passed static data through the system. Used default styles and functionality from https://gist.githubusercontent.com/davidvandusen/62353f9355951889d79f04e9e1bc07ed/raw/868a24bd53e279db731b7a685d2a85d12450f545/static_chatty.html

---
+++ src/MessageList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">
+        {this.props.messages.map( (message)=> {return <Message key={message.id} username= {message.username} content= {message.content}/>})}
+      </main>
+    );
+  }
+}
+
+export default MessageList;
